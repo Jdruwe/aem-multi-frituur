2db94837582eeb4802cb90446094007f17919759
jdruwe <druwe.jeroen@gmail.com>
2022-02-17 14:41:56 +0100

    Added yarn workspaces

---
+++ packages/ui-common/index.ts
@@ -0,0 +1,5 @@
+const sayHello = () => {
+    console.log('>>> Hello from ui-common!');
+}
+
+export default sayHello;
--- tenant.haag-4/frontend/src/main.ts
+++ tenant.haag-4/frontend/src/main.ts
@@ -1,6 +1,9 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import sayHello from '@jeroendruwe/ui-common';
 
 console.log('>>> main called!');
 
 createApp(App).mount('#app');
+
+sayHello();
