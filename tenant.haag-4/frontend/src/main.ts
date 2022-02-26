bc541697d4b8dad8fc4b29ff84f89a198004341b
jdruwe <druwe.jeroen@gmail.com>
2022-02-26 22:08:34 +0100

    VueBootstrap.ts WIP

--- tenant.haag-4/frontend/src/main.ts
+++ tenant.haag-4/frontend/src/main.ts
@@ -1,12 +1,3 @@
-// import {createApp} from 'vue'
-// import App from './App.vue'
-//
-// console.log('>>> main called!');
-//
-// createApp(App).mount('#app');
-//
-// init({});
-
 import {ModulePromises, VueBootstrap} from '@jeroendruwe/ui-common';
 
 const modulePromises: ModulePromises = {
